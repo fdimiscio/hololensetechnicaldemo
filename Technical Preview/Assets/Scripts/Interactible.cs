4e137d54e2b8e1c254c72b5cdc1b812da93c2c91
Daniel Tonks <daniel.tonks.3d@gmail.com>
2016-07-29 11:46:59 +0100

    Refactored interactable, introduced emulated Unity methods for editor testing.

--- Technical Preview/Assets/Scripts/Interactible.cs
+++ Technical Preview/Assets/Scripts/Interactible.cs
@@ -4,15 +4,33 @@
 /// The Interactible class flags a Game Object as being "Interactible".
 /// Determines what happens when an Interactible is being gazed at.
 /// </summary>
-public class Interactible : MonoBehaviour
-{
+public class Interactible : CoreComponent {
+
+    //---------------------------------------------
+    // Constants:
+    //---------------------------------------------
+
+    private const string Metallic = "_Metallic";
+
+    //---------------------------------------------
+    // Public Variables:
+    //---------------------------------------------
+
     [Tooltip("Audio clip to play when interacting with this hologram.")]
     public AudioClip TargetFeedbackSound;
-    private AudioSource audioSource;
 
+    //---------------------------------------------
+    // Private Variables:
+    //---------------------------------------------
+
+    private AudioSource audioSource;
     private Material[] defaultMaterials;
 
-    void Start()
+    //---------------------------------------------
+    // Unity Methods:
+    //---------------------------------------------
+
+    protected void Start()
     {
         defaultMaterials = GetComponent<Renderer>().materials;
 
@@ -25,6 +43,53 @@ public class Interactible : MonoBehaviour
 
         EnableAudioHapticFeedback();
     }
+
+#if UNITY_EDITOR
+    protected void OnMouseEnter() {
+        GazeEntered();
+    }
+
+    protected void OnMouseExit() {
+        GazeExited();
+    }
+
+    protected void OnMouseDown() {
+        OnSelect();
+    }
+#endif
+
+    //---------------------------------------------
+    // Hololense Methods:
+    //---------------------------------------------
+
+    protected void GazeEntered() {
+        for (int i = 0; i < defaultMaterials.Length; i++) {
+            defaultMaterials[i].SetFloat(Metallic, .5f);
+        }
+    }
+
+    protected void GazeExited() {
+        for (int i = 0; i < defaultMaterials.Length; i++) {
+            defaultMaterials[i].SetFloat(Metallic, 0f);
+        }
+    }
+
+    protected void OnSelect() {
+        for (int i = 0; i < defaultMaterials.Length; i++) {
+            defaultMaterials[i].SetFloat(Metallic, 1f);
+        }
+
+        // Play the audioSource feedback when we gaze and select a hologram.
+        if (audioSource != null && !audioSource.isPlaying) {
+            audioSource.Play();
+        }
+
+        SendMessage("PerformTagAlong"); // todo: work out what this is sending to, should avoid using SendMessage and use events in the hierarchy instead, much easier to trace and debug.
+    }
+
+    //---------------------------------------------
+    // Private Methods:
+    //---------------------------------------------
 
     private void EnableAudioHapticFeedback()
     {
@@ -42,37 +107,5 @@ public class Interactible : MonoBehaviour
             audioSource.spatialBlend = 1;
             audioSource.dopplerLevel = 0;
         }
-    }
-
-    void GazeEntered()
-    {
-        for (int i = 0; i < defaultMaterials.Length; i++)
-        {
-            defaultMaterials[i].SetFloat("_Metallic", .5f);
-        }
-    }
-
-    void GazeExited()
-    {
-        for (int i = 0; i < defaultMaterials.Length; i++)
-        {
-            defaultMaterials[i].SetFloat("_Metallic", 0f);
-        }
-    }
-
-    void OnSelect()
-    {
-        for (int i = 0; i < defaultMaterials.Length; i++)
-        {
-            defaultMaterials[i].SetFloat("_Metallic", 1f);
-        }
-
-        // Play the audioSource feedback when we gaze and select a hologram.
-        if (audioSource != null && !audioSource.isPlaying)
-        {
-            audioSource.Play();
-        }
-
-        this.SendMessage("PerformTagAlong");
     }
 }
